c1eb555959b2b6e1f0529f6fcba0e7a021204dae
Joseph Cecil <jcecil@isi.edu>
2022-10-31 23:36:47 -0400

    Fix old assert message

--- src/asr.c
+++ src/asr.c
@@ -5,7 +5,8 @@ int asr(int x, int y) {
     assert(y >= 0 && "y < 0 triggers undefined behavior.");
     assert(
         y < CHAR_BIT * sizeof(x) && 
-        "y greater than the (bit) width of x triggers undefined behavior."
+        "y greater than or equal to the (bit) width of x triggers " &&
+        "undefined behavior."
     );
     unsigned int extended_sign = (x >= 0) ? 0 : ~(UINT_MAX >> y);
     unsigned int reinterpreted_x = *(unsigned int*)(&x);
